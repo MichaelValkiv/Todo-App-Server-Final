a52fa150956e7ddc2af5ecb56827b924e4817d73
Misha Valkiv <mishavalkiv@gmail.com>
2020-03-16 18:23:21 +0200

    Update sender.js

--- sender.js
+++ sender.js
@@ -1,16 +1,13 @@
 const firebase = require('firebase-admin');
 
-const serviceAccount = require('./todo-list-app.json');
+const serviceAccount = require('./todo-list-app-ba9d7-firebase-adminsdk-tlrrv-3717943956.json');
 
 const firebaseToken = 'AAAARqoUJrw:APA91bGfowBw3zojJoZ45v0fmX9iUKuvlnsChWDUzdK4NsI7Koyds5eJPCodiKAAF_WVP3s8iX9ZkfhJ9L83Jy-VosI7HfMEfjaGHnHxitZdLM-iHqbQUTxmCRqkiqM1Uv4QOUqfW6g1';
 
 firebase.initializeApp({
-   credential: firebase.credential.cert(serviceAccount),
-   databaseURL: ''
+    credential: firebase.credential.cert(serviceAccount),
+    databaseURL: 'https://todo-list-app-ba9d7.firebaseio.com'
 });
-
-const messaging = firebase.messaging();
-
 
 const payload = {
     notification: {
@@ -24,4 +21,14 @@ const options = {
     timeToLive: 60,
 };
 
-firebase.messaging().sendToDevice(firebaseToken, payload, options);
+setTimeout(() => {
+    firebase.messaging().sendToDevice(firebaseToken, payload, options).then(
+        (response) => {
+            console.log('done:', response);
+            console.log(response.results[0].error);
+        },
+        (error) => {
+            console.log('error:', error);
+        }
+    );
+}, 5000);
