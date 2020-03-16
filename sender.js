4e5b0728cacdaaf8d654d630c120336dd28e89c8
Misha Valkiv <mishavalkiv@gmail.com>
2020-03-16 16:51:18 +0200

    sender update

--- sender.js
+++ sender.js
@@ -2,7 +2,7 @@ const firebase = require('firebase-admin');
 
 const serviceAccount = require('./todo-list-app.json');
 
-const firebaseToken = '';
+const firebaseToken = 'AAAARqoUJrw:APA91bGfowBw3zojJoZ45v0fmX9iUKuvlnsChWDUzdK4NsI7Koyds5eJPCodiKAAF_WVP3s8iX9ZkfhJ9L83Jy-VosI7HfMEfjaGHnHxitZdLM-iHqbQUTxmCRqkiqM1Uv4QOUqfW6g1';
 
 firebase.initializeApp({
    credential: firebase.credential.cert(serviceAccount),
