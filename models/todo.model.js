98573fbddec64112e74126bd7973afb59c51b05b
Misha Valkiv <mishavalkiv@gmail.com>
2020-03-18 11:03:30 +0200

    update

--- models/todo.model.js
+++ models/todo.model.js
@@ -5,18 +5,11 @@ const helper = require('../helpers/helper.js');
 
 const firebase = require('firebase-admin');
 const serviceAccount = require('../todo-list-app-ba9d7-firebase-adminsdk-tlrrv-3717943956.json');
-const firebaseToken = 'AAAARqoUJrw:APA91bGwv0YTitpUJX8Ol5K9hDSQjUpMAxILYok0Qfwe-1Yk_7vgrPMNJn916pweLLSh_d64PWOjGf2wafZvGL1Vof6JrIOTZ3Dgr-MG8LXbN--vrOZ27jWORsEym29cHGLtDR_RxX7-';
+const firebaseToken = 'fqtRMhMRgR0:APA91bGk5jHMhWxk_8inU07H_A91igLt1Om_jIoqlYzOE4IoqLz6k6i9p7dcQuB2XPTEym_GiWjw30VY-8KnBhPRoOQ7XZe5r8GhWVKkuAkfT2gomCZHm7jFo5mDTiuh2FkwkSi6CI2X';
 firebase.initializeApp({
     credential: firebase.credential.cert(serviceAccount),
     databaseURL: 'https://todo-list-app-ba9d7.firebaseio.com'
 });
-
-const payload = {
-    notification: {
-        title: 'From Node.js server',
-        body: 'Hello user!!!',
-    }
-};
 
 const options = {
     // priority: 'high',
@@ -55,6 +48,13 @@ function insertTodo(newTodo) {
         todos.push(newTodo);
         helper.writeJSONFile(filename2, todos);
         resolve(newTodo);
+
+        const payload = {
+            notification: {
+                title: newTodo.name,
+                body: 'Must be done to ' + newTodo.deadline,
+            }
+        };
 
         setTimeout(() => {
             firebase.messaging().sendToDevice(firebaseToken, payload, options).then(
--- sender.js
+++ sender.js
@@ -2,7 +2,7 @@ const firebase = require('firebase-admin');
 
 const serviceAccount = require('./todo-list-app-ba9d7-firebase-adminsdk-tlrrv-3717943956.json');
 
-const firebaseToken = 'AAAARqoUJrw:APA91bGfowBw3zojJoZ45v0fmX9iUKuvlnsChWDUzdK4NsI7Koyds5eJPCodiKAAF_WVP3s8iX9ZkfhJ9L83Jy-VosI7HfMEfjaGHnHxitZdLM-iHqbQUTxmCRqkiqM1Uv4QOUqfW6g1';
+const firebaseToken = 'fqtRMhMRgR0:APA91bGk5jHMhWxk_8inU07H_A91igLt1Om_jIoqlYzOE4IoqLz6k6i9p7dcQuB2XPTEym_GiWjw30VY-8KnBhPRoOQ7XZe5r8GhWVKkuAkfT2gomCZHm7jFo5mDTiuh2FkwkSi6CI2X';
 
 firebase.initializeApp({
     credential: firebase.credential.cert(serviceAccount),
